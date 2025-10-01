e2b67e105ad435ca643e9acd0e1352f7b547f4b8
p2002814 <edgar.demeude@etu.univ-lyon1.fr>
2025-10-02 01:12:31 +0200

    changed api url

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # allow all origins
     allow_credentials=True,
-    allow_methods=["*"], # GET, POST, PUT, DELETE...
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
